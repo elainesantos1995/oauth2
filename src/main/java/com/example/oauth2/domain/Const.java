d7a4eb4ac05fb22e4df75d0b7aa51df90eaaf3b5
Elaine Silva <elaine.ssantos.2013@gmail.com>
2023-02-05 19:53:32 -0300

    commit inicial

---
+++ src/main/java/com/example/oauth2/domain/Const.java
@@ -0,0 +1,7 @@
+package com.example.oauth2.domain;
+
+public class Const {
+    
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+    public static final String ROLE_CLIENT = "ROLE_CLIENT";
+}
